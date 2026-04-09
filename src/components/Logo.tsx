cf93982d06391eae3225b8aeec085080cac687da
influencee-dev <influencee2019@gmail.com>
2026-04-09 16:22:36 +0200

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -51,7 +51,7 @@ export const LogoReveal = () => {
           <img 
             src="/new.jpg" 
             alt="New Alfadocs Logo" 
-            className="w-50 h-50 md:w-24 md:h-24 object-contain drop-shadow-[0_0_32px_rgba(103,97,229,0.6)]"
+            className="w-50 h-50 md:w-60 md:h-60 object-contain drop-shadow-[0_0_32px_rgba(103,97,229,0.6)]"
             referrerPolicy="no-referrer"
           />
         
